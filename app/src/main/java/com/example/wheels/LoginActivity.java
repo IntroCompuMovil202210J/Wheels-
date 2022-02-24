0b4ce6913c4a9db54005a95aebf3230cf58136db
Fabian Olarte <fabianx030x@gmail.com>
2022-02-23 19:19:50 -0500

    pantalla inicio sesión terminada y avances de pantalla tipo de usuario

--- app/src/main/java/com/example/wheels/LoginActivity.java
+++ app/src/main/java/com/example/wheels/LoginActivity.java
@@ -2,8 +2,11 @@ package com.example.wheels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +15,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        //Variables
+        TextView textRegistroB = findViewById(R.id.textRegistroB);
+
+        textRegistroB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
