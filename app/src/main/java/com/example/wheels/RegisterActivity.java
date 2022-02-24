795a156eb8cc5536acacfda3b758a3b9eab20266
Fabian Olarte <fabianx030x@gmail.com>
2022-02-23 20:17:10 -0500

    Pantallas registro conductor

--- app/src/main/java/com/example/wheels/RegisterActivity.java
+++ app/src/main/java/com/example/wheels/RegisterActivity.java
@@ -2,7 +2,11 @@ package com.example.wheels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,6 +14,45 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //variables
+        TextView textAnteriorPantalla = findViewById(R.id.textAnteriorPantalla);
+        TextView textContinuarPantalla  = findViewById(R.id.textContinuarPantalla);
+        Button botonTipoConductor = findViewById(R.id.botonTipoConductor);
+        Button botonTipoPasajero = findViewById(R.id.botonTipoPasajero);
+
+
+        //Anterior
+        textAnteriorPantalla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentVolver = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intentVolver);
+                finish();
+            }
+        });
+
+        //Boton conductor seleccionado
+        botonTipoConductor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        textContinuarPantalla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Colocar logica para saber si el boton fue seleccionado o no (condicional)
+                Intent intentContinuar = new Intent(RegisterActivity.this, RegisterDatosPersonalesActivity.class);
+                startActivity(intentContinuar);
+                finish();
+
+            }
+        });
+
+
 
 
     }
